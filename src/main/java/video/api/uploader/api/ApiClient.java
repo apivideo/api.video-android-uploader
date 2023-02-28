0085fe8473e27a2487cff3fead0b198bda08b901
ThibaultBee <ThibaultBee@users.noreply.github.com>
2023-02-28 10:45:14 +0000

    chore(config): bump android and uploader for new WorkManager API

--- src/main/java/video/api/uploader/api/ApiClient.java
+++ src/main/java/video/api/uploader/api/ApiClient.java
@@ -118,7 +118,7 @@ public class ApiClient {
     private void init() {
         verifyingSsl = true;
         json = new JSON();
-        addDefaultHeader("AV-Origin-Client", "android-uploader:1.1.2");
+        addDefaultHeader("AV-Origin-Client", "android-uploader:1.2.0");
     }
 
     private boolean isValid(String regex, String field) {
