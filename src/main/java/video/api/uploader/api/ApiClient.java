313df7e0b57a6c414ebe71f47a8aeb17fa17425e
ThibaultBee <ThibaultBee@users.noreply.github.com>
2023-08-21 14:04:19 +0000

    feat(java): improve cancellation of WorkManager uploads

--- src/main/java/video/api/uploader/api/ApiClient.java
+++ src/main/java/video/api/uploader/api/ApiClient.java
@@ -118,7 +118,7 @@ public class ApiClient {
     private void init() {
         verifyingSsl = true;
         json = new JSON();
-        addDefaultHeader("AV-Origin-Client", "android-uploader:1.2.4");
+        addDefaultHeader("AV-Origin-Client", "android-uploader:1.3.0");
     }
 
     private boolean isValid(String regex, String field) {
