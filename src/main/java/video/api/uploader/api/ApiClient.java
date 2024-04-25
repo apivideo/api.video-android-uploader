17ca534a2b5d75ff095a14f51d3354771e90f2ae
ThibaultBee <ThibaultBee@users.noreply.github.com>
2024-04-25 13:17:26 +0000

    chore(config): bump java and swift clients/uploaders version

--- src/main/java/video/api/uploader/api/ApiClient.java
+++ src/main/java/video/api/uploader/api/ApiClient.java
@@ -118,7 +118,7 @@ public class ApiClient {
     private void init() {
         verifyingSsl = true;
         json = new JSON();
-        addDefaultHeader("AV-Origin-Client", "android-uploader:1.3.5");
+        addDefaultHeader("AV-Origin-Client", "android-uploader:1.3.6");
     }
 
     private boolean isValid(String regex, String field) {
